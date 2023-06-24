0840c488cbd0c379e2b5fed8127d368978f7f905
alaa-jamal <alaa99702@gmail.com>
2023-06-24 18:28:49 +0300

    create QuizApp

---
+++ question.js
@@ -0,0 +1,87 @@
+data = [
+  {
+    question:
+      "Which HTML element is used to specify a header for a document or section?",
+    answers: ["<section>", "<top>", "<header>"],
+
+    correctAnswer: "<header>",
+  },
+
+  {
+    question: "What does Css stand for?",
+    answers: [
+      "cascading style sheet",
+      " caleful style sheet",
+      "computer style sheet",
+    ],
+
+    correctAnswer: "cascading style sheet",
+  },
+
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      "HyperLink and Markup language.",
+      "Hyper Text Markup Language.",
+      "Home Tool Markup Language.",
+    ],
+
+    correctAnswer: "Hyper Text Markup Language.",
+  },
+
+  {
+    question: "Which HTML element defines navigation links?",
+    answers: ["navigate", "navigation", "nav"],
+
+    correctAnswer: "nav",
+  },
+
+  {
+    question:
+      "In HTML, which attribute is used to specify that an input field must be filled out?",
+    answers: ["required", "validate", "Placeholder"],
+
+    correctAnswer: "required",
+  },
+
+  {
+    question:
+      "Which HTML element is used to specify a footer for a document or section?",
+    answers: ["footer", "section", "bootom"],
+
+    correctAnswer: "footer",
+  },
+
+  {
+    question:
+      "Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?",
+    answers: ["alt", "title", "src"],
+
+    correctAnswer: "alt",
+  },
+
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      "HyperLink and Markup language.",
+      "Hyper Text Markup Language.",
+      "Home Tool Markup Language.",
+    ],
+
+    correctAnswer: "HyperLink and Markup language.",
+  },
+
+  {
+    question: "Choose the correct HTML element for the largest heading:",
+    answers: ["<head>", "<h1>", "<heading>"],
+
+    correctAnswer: "<h1>",
+  },
+
+  {
+    question: "Choose the correct HTML element to define important text?",
+    answers: ["<i>", "<strong>", "<b>"],
+
+    correctAnswer: "<strong>",
+  },
+];
